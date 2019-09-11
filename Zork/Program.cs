6d76672c1bc67fe4882daef86876a7f6b62fa243
Bollesyy <33631907+Bollesyy@users.noreply.github.com>
2019-09-11 13:57:55 -0400

    Finished 2&3
    Looking around and Quitting
    Finding Your Direction

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,11 +2,53 @@
 
 namespace Zork
 {
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
+
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+
+            Commands command;
+
+            if (commandString == "QUIT")
+            {
+                command = Commands.QUIT;
+            }
+            else if (commandString == "LOOK")
+            {
+                command = Commands.LOOK;
+            }
+            else if (commandString == "NORTH")
+            {
+                command = Commands.NORTH;
+            }
+            else if (commandString == "SOUTH")
+            {
+                command = Commands.SOUTH;
+            }
+            else if (commandString == "EAST")
+            {
+                command = Commands.EAST;
+            }
+            else if (commandString == "WEST")
+            {
+                command = Commands.WEST;
+            }
+            else
+            {
+                command = Commands.UNKNOWN;
+            }
+            return command;
         }
     }
 }
